cd4393f5149570656e74796a45ba5821eb01af6a
Shankar-Sala <shankarsala9995@gmail.com>
2025-08-19 19:19:59 +0530

    PQ-5

---
+++ 04.Loops/PQ5.java
@@ -0,0 +1,11 @@
+public class PQ5 {
+    public static void main(String[] args) {
+        // int i;
+        // for ( i = 0; i <= 5; i++) {
+        for (int i = 0; i <= 5; i++) {
+         System.out.println("i = "+i);   
+        }
+
+        // System.out.println("i after the loop = "+ i); // it gives error because the "i" is not declare or initialize.
+    }
+}
