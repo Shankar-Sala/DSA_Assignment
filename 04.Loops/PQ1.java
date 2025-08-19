995364a9d272fb276fce942fe83fb2a83cb22a29
Shankar-Sala <shankarsala9995@gmail.com>
2025-08-19 18:44:10 +0530

    Pq-1 update

--- 04.Loops/PQ1.java
+++ 04.Loops/PQ1.java
@@ -3,9 +3,20 @@
 
 public class PQ1{
     public static void main(String args[]){
-        for(int i=0; i<5; i++){
+
+        //! Method-1 for-loop 
+        // for(int i=0; i<5; i++){
+        //     System.out.println("Hello");
+        //     i+=2;
+        // }
+
+        // ! Method-2 while loop
+        int i=0;
+        while(i<5){
             System.out.println("Hello");
-            i+=2;
+            i += 2;  // method-1
+            i++;  
+            // i +=3;   //  method-2
         }
     }
 }
