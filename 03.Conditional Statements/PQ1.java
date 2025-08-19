04d8bef5fdc04ec7f357128a76c79ff6aaf26178
Shankar-Sala <shankarsala9995@gmail.com>
2025-08-19 15:20:51 +0530

    Pq-1 Revision

--- 03.Conditional Statements/PQ1.java
+++ 03.Conditional Statements/PQ1.java
@@ -1,16 +1,38 @@
+
 import java.util.*;
 
-public class PQ1{
-  public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter a Number: ");
-    int number = sc.nextInt();
+public class PQ1 {
 
-    if(number > 0){
-      System.out.println("Positive Number");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //!using If-else statements
+        // int num = sc.nextInt();
+        // if(num == 0){
+        //     System.out.println("The number is 0.");
+        // }
+        // if(num < 0){
+        //     System.out.println("The number is Negative");
+        // }else {
+        //     System.out.println("The number is Positive");
+        // }
+        // ! Using switch Statement
+        int num = sc.nextInt();
+        int y = num > 0 ? 1 : -1;
+
+        if (num == 0) {
+            y = 0;
+        }
+        switch (y) {
+            case 0:
+                System.out.println("The number is 0.");
+                break;
+            case 1:
+                System.out.println("The number is Positive");
+                break;
+            case -1:
+                System.out.println("The number is Negative");
+               
+        }
     }
-    else{
-      System.out.println("Negative Number");
-    }
-  }
 }
