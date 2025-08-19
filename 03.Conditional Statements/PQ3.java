a3d82c60d60ef4417fa4bf728a4551ae81f93e22
Shankar-Sala <shankarsala9995@gmail.com>
2025-08-19 15:41:42 +0530

    Pq-3 revision

--- 03.Conditional Statements/PQ3.java
+++ 03.Conditional Statements/PQ3.java
@@ -1,27 +1,37 @@
+
 import java.util.*;
 
-public class PQ3{
-  public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter Week Number: ");
-    int week = sc.nextInt();
+public class PQ3 {
 
-    switch(week){
-      case 1: System.out.print("Sunday");
-      break;
-      case 2: System.out.print("Monday");
-      break;
-      case 3: System.out.print("Tuesday");
-      break;
-      case 4: System.out.print("Wednesday");
-      break;
-      case 5: System.out.print("Thursday");
-      break;
-      case 6: System.out.print("Friday");
-      break;
-      case 7: System.out.print("Saturday");
-      break;
-      default:System.out.print("Invalid input! Please enter week number between(1-7)");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Week Number: ");
+        int day = sc.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.print("Sunday");
+                break;
+            case 2:
+                System.out.print("Monday");
+                break;
+            case 3:
+                System.out.print("Tuesday");
+                break;
+            case 4:
+                System.out.print("Wednesday");
+                break;
+            case 5:
+                System.out.print("Thursday");
+                break;
+            case 6:
+                System.out.print("Friday");
+                break;
+            case 7:
+                System.out.print("Saturday");
+                break;
+            default:
+                System.out.print("Invalid input! Please enter week number between(1-7)");
+        }
     }
-  }
 }
