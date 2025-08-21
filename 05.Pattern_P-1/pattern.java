d35cef9a413fb67e9a3fa1c4ddac9919129a2a66
Shankar-Sala <shankarsala9995@gmail.com>
2025-08-21 11:14:21 +0530

    PQ-1

---
+++ 05.Pattern_P-1/pattern.java
@@ -0,0 +1,55 @@
+
+import java.util.Scanner;
+
+public class pattern {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        //* for loop 
+        // int n = sc.nextInt();
+        
+        // int st = 1;
+        // int sp = n - 1;
+        // for(int i = 0; i<=n; i++){
+        //    //print stars
+        // for(int j=0; j<st; j++){
+        //     System.out.print("* ");
+        // }
+        //    //print spaces
+        //    for(int j=0; j<sp; j++){
+        //     System.out.print(" ");
+        // }
+
+        //    //prepare for next line
+        //    System.out.println();
+        //    st++;
+        //    sp--;
+        // }
+
+        //* While loop 
+        // int n = sc.nextInt();
+        // int st = 1;
+        // int sp = n - 1;
+        // int number_of_line = 1;
+        
+        // while(number_of_line < n){
+                  
+        // for(int i = 0; i<=n; i++){
+        //    //print stars
+        // for(int j=0; j<st; j++){
+        //     System.out.print("* ");
+        // }
+        //    //print spaces
+        //    for(int j=0; j<sp; j++){
+        //     System.out.print(" ");
+        // }
+
+        //    //prepare for next line
+        //    System.out.println();
+        //    st++;
+        //    sp--;
+        //    number_of_line++;
+        // }
+        // }
+
+    }
+}
